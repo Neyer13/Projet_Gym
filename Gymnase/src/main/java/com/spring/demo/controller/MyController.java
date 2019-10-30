c981bc12dae2fc744a0da8abcff0c0dd49add952
Neyer Crismar VALERIANO RODRIGUEZ <n.valeriano@groupeonepoint.com>
2019-10-30 14:17:17 +0100

    Creation des liens entre les pages a partir des boutons

---
+++ Gymnase/src/main/java/com/spring/demo/controller/MyController.java
@@ -0,0 +1,37 @@
+package com.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MyController {
+	@RequestMapping(value ="/", method=RequestMethod.GET)
+	public String homeAmpty() {
+		return"home";
+	}
+	@RequestMapping(value ="/home", method=RequestMethod.GET)
+	public String home() {
+		return"home";
+	}
+	
+	@RequestMapping(value = "/sport")
+	public String sport() {
+		return"Sport";
+	}
+	
+	@RequestMapping(value = "/gymnase")
+	public String gymnase() {
+		return"gymnase";
+	}
+	@RequestMapping(value = "/ville")
+	public String ville() {
+		return"Ville";
+	}
+
+	@RequestMapping(value = "/jour")
+	public String jour() {
+		return"Jour";
+	}
+	
+}
